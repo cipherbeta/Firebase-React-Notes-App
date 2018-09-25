da636a66855adcd7ae4eb0bc98b7ca3b7ba6cc41
Isaac Doud <ikedoud45@gmail.com>
2018-09-25 17:48:01 -0500

    add post

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## Notesapp Clone
+# Notes App
 
 I was getting sick with some of the issues with evernote that I keep running into, so I figured I'd try my hand at making my own version of the app. Uses Firebase + React.
--- src/components/common/addbar.js
+++ src/components/common/addbar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {database} from '../../firebase/config';
 
 const AddBar = () => {
+    let AddNewPost = () => {
+        database.ref('notes/').push({
+            title: "New Note",
+            tags: null,
+            content: null
+        });
+        console.log("added")
+    }
     return(
         <aside id="addbar_wrapper">
-            <i className="fas fa-plus-circle"/>
+            <i className="fas fa-plus-circle" onClick={()=>AddNewPost()}/>
             <i className="fas fa-minus-circle"/>
         </aside>
     );
